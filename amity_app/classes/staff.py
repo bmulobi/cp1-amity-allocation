83762ba7e540078f0240748006abe41109a87454
bernard mulobi <bmulobi@gmail.com>
2017-03-25 16:09:15 +0300

    modify methods

--- amity_app/classes/staff.py
+++ amity_app/classes/staff.py
@@ -1,23 +1,21 @@
 from amity_app.classes.person import Person
 
-
-
 class Staff(Person):
     """
     inherits from Person
     """
 
-    def __init__(self, name, role, accommodation="N"):
+    def __init__(self, role="STAFF"):
+
         """
         :type accommodation: str
         :type name: str
         :type role: str
         """
-
-        self.name = name
-        self.person_role = role
-        self.accommodation = accommodation
-
-    def add_person(self):
-        return "add_person() was called successfully from class Staff"
         
+        self.name = ""
+        self.person_role = role
+        self.person_id = ""
+
+    def add_person(self, name):
+        return self.person_id, name + " was added successfully"
