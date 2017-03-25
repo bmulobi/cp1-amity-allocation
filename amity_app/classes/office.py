3bc80d34670e77487389e02b5a59c0ef340f45d5
bernard mulobi <bmulobi@gmail.com>
2017-03-25 16:06:23 +0300

    modify methods

--- amity_app/classes/office.py
+++ amity_app/classes/office.py
@@ -5,21 +5,21 @@ class Office(Room):
     child class
     """
 
-    def __init__(self, room_name, room_type="office",room_capacity=6):
+    def __init__(self, room_type="office", room_capacity=6):
+
         """
 
         :param room_name:
         :param room_type:
         """
 
-        self.room_name = room_name
-        self.room_type = room_type
-        self.room_capacity = room_capacity
+        self.room_name = ""
 
+    def create_room(self, name):
 
-    def create_room(self):
         """
 
         :return:
         """
         return "create_room() was called successfully from class Office"
+ 
