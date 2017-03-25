203c34ec467b2d290ec2ac03ce86a6954435fca4
bernard mulobi <bmulobi@gmail.com>
2017-03-25 16:05:43 +0300

    modify methods

--- amity_app/classes/living_space.py
+++ amity_app/classes/living_space.py
@@ -6,20 +6,18 @@ class LivingSpace(Room):
     child class
     """
 
-    def __init__(self, room_name, room_type="living_space", room_capacity=4):
+
+    def __init__(self, room_type="living_space", room_capacity=4):
+
         """
 
-        :param room_name:
         :param room_type:
         """
 
-        self.room_name = room_name
+        self.room_name = ""
         self.room_type = room_type
         self.room_capacity = room_capacity
 
-    def create_room(self):
-        """
+    def create_room(self, room_name):
 
-        :return:
-        """
-        return "create_room() was called successfully from class LivingSpace"
+        return room_name + " was created successfully"
