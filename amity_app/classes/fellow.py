db58031f254366571f15d2062dfac7628ab331a5
bernard mulobi <bmulobi@gmail.com>
2017-03-27 19:50:29 +0300

    Modify method

--- amity_app/classes/fellow.py
+++ amity_app/classes/fellow.py
@@ -1,4 +1,5 @@
 from amity_app.classes.person import Person
+from amity_app.classes.amity import Amity
 
 
 class Fellow(Person):
@@ -10,6 +11,10 @@ class Fellow(Person):
         self.name = ""
         self.person_role = role
         self.accommodation = accommodation
+        self.person_id = ""
 
-    def add_person(self, name):
-        return person_id, name + " was added successfully"
+    def add_person(self, name, wants_accomodation):
+        Amity.person_identifier += 1
+        self.person_id = "f" + str(Amity.person_identifier)
+        self.name = name
+        return self.person_id, self.name + " was added successfully"
