5fdf343221c89f2161588ed82ea65984beaa3eaa
bernard mulobi <bmulobi@gmail.com>
2017-03-27 19:49:46 +0300

    Add more methods

--- amity_app/classes/amity.py
+++ amity_app/classes/amity.py
@@ -9,8 +9,8 @@ class Amity(object):
 
     # class properties
     people_list = [{}, {}]
-    rooms_list = [{},{}]
-    space_is_available_flag = False
+    rooms_list = [{}, {}]
+    person_identifier = 0
 
     def __init__(self):
         """
@@ -18,7 +18,6 @@ class Amity(object):
         :param person_name:
         """
 
-        self.person_identifier = ""
         self.new_room_name = ""
         self.allocated_file_name = ""
         self.unallocated_file_name = ""
@@ -30,6 +29,9 @@ class Amity(object):
         self.offices_list = []
         self.fellows_list = []
         self.staff_list = []
+        self.matched_identifiers_list = []
+        self.staff_identifiers_list = []
+        self.fellows_identifiers_list = []
 
     def confirm_person_identifier(self, person_identifier):
         """
@@ -58,7 +60,6 @@ class Amity(object):
         if len(Amity.rooms_list[0]):
             self.living_spaces_list = Amity.rooms_list[0].keys()
 
-
         if len(Amity.rooms_list[1]):
             self.offices_list = Amity.rooms_list[1].keys()
 
@@ -74,9 +75,9 @@ class Amity(object):
 
         return new_room_name in self.living_spaces_list or new_room_name in self.offices_list
 
+    # Confirms whether specific room has space for further allocations
     def confirm_specific_room_has_space(self, room_name):
         """
-
         :param room_name:
         :return:
         """
@@ -88,6 +89,57 @@ class Amity(object):
         if room_name in self.living_spaces_list:
             return len(Amity.rooms_list[0][room_name]) < 4
 
+    # Fetches room type
+    def fetch_room_type(self, room_name):
+        """
+        """
+        self.offices_list, self.living_spaces_list = self.search_rooms_list()
+        if room_name in self.offices_list:
+            return "office"
+        if room_name in self.living_spaces_list:
+            return "livingspace"
+
+    # Alerts if we try to allocate person to the same room they are already allocated
+    def confirm_person_not_doubly_reallocated_to_same_room(self,
+                                                           person_identifier,
+                                                           room_name):
+        """
+        Returns:
+        True -  if we try to allocate person to a room
+                where they are already allocated.
+        False - otherwise.
+        """
+
+        if (self.fetch_room_type(room_name) == "office"):
+
+            if person_identifier in Amity.rooms_list[1][room_name]:
+                return True
+        else:
+            if person_identifier in Amity.rooms_list[0][room_name]:
+                return True
+        return False
+
+    def fetch_person_identifier(self, person_name, role):
+        """
+        returns: uses name and role to
+                 return associated person identifier
+        """
+        people_list = [{}, {}]
+        self.matched_identifiers_list = []
+        if role == "staff":
+            self.staff_identifiers_list = Amity.people_list[1].keys()
+            for identifier in self.staff_identifiers_list:
+                if person_name in Amity.people_list[1][identifier]:
+                    self.matched_identifiers_list.append(identifier)
+
+        else:
+            self.fellows_identifiers_list = Amity.people_list[0].keys()
+            for identifier in self.fellows_identifiers_list:
+                if person_name in Amity.people_list[0][identifier]:
+                    self.matched_identifiers_list.append(identifier)
+
+        return self.matched_identifiers_list
+
     def reallocate_person(self, person_identifier, new_room_name):
         """
 
@@ -96,7 +148,7 @@ class Amity(object):
         :return:
         """
 
-        return "Person identifier is : "+str(person_identifier)+" and new room name is: "+new_room_name
+        return "Person with identifier " + person_identifier + " was reallocated to " + new_room_name
 
     def confirm_availability_of_space_in_amity(self):
         """
@@ -105,34 +157,124 @@ class Amity(object):
         """
 
         if len(Amity.rooms_list[0]) > 0:
-            for item in Amity.rooms_list[0].iteritems():
+            items_list = Amity.rooms_list[0].values()
+            for item in items_list:
                 if len(item) < 4:
                     return True
         if len(Amity.rooms_list[1]) > 0:
-            for item in Amity.rooms_list[1].iteritems():
+            items_list = Amity.rooms_list[1].values()
+            for item in items_list:
                 if len(item) < 6:
                     return True
         return False
 
+    def fetch_rooms_with_space(self):
+        """
+        returns 2 lists: one for offices with space (if any)
+        and one for livingspaces with space (if any) and
+        the number of spaces available in each room
+        """
+        offices_list, living_spaces_list = self.search_rooms_list()
+        offices_with_space = []
+        living_spaces_with_space = []
+
+        if len(offices_list):
+
+            for room in offices_list:
+                space = 0
+                space = 6 - len(Amity.rooms_list[1][room])
+                if space:
+                    offices_with_space.append([room, space])
+
+        if len(living_spaces_list):
+
+            for room in living_spaces_list:
+                space = 0
+                space = 4 - len(Amity.rooms_list[0][room])
+                if space:
+                    living_spaces_with_space.append([room, space])
+        return offices_with_space, living_spaces_with_space
+
     def load_people(self):
         """
-
         :param file_path:
         :return:
         """
+        offices, living_spaces = self.fetch_rooms_with_space()
+        if len(offices) or len(living_spaces):
+            try:
+                file_object = open("../text_files/test_file.txt", "r")
+                try:
+                    lines_list = file_object.readlines()
+                    for line in lines_list:
+                        Amity.person_identifier += 1
+                        line_fragments = line.split()
+                        if len(line_fragments) == 3:
+                            if line_fragments[2] == "FELLOW":
+                                Amity.people_list[0]\
+                                    ["f-" + str(Amity.person_identifier)] \
+                                    = [line_fragments[0]
+                                       + " " + line_fragments[1]]
+                                for room in offices:
+                                    if room[1] > 0:
+                                        Amity.rooms_list[1][room[0]].append("f-" + str(Amity.person_identifier))
+                                        room[1] -= 1
+                                        break
+                            else:
+                                Amity.people_list[1]\
+                                    ["s-" + str(Amity.person_identifier)] \
+                                    = [line_fragments[0]
+                                       + " " + line_fragments[1]]
+                                for room in offices:
+                                    if room[1] > 0:
+                                        Amity.rooms_list[1][room[0]].append("s-" + str(Amity.person_identifier))
+                                        room[1] -= 1
+                                        break
+                        if len(line_fragments) == 4:
+                            Amity.people_list[0]\
+                                ["f-" + str(Amity.person_identifier)] \
+                                = [line_fragments[0]
+                                   + " " + line_fragments[1]]
+                            for room in offices:
+                                if room[1] > 0:
+                                    Amity.rooms_list[1][room[0]].append("f-" + str(Amity.person_identifier))
+                                    room[1] -= 1
+                                    break
+                            for room in living_spaces:
+                                if room[1] > 0:
+                                    Amity.rooms_list[0][room[0]].append("f-" + str(Amity.person_identifier))
+                                    room[1] -= 1
+                                    break
 
-        return "load_people() was called successfully "
+                finally:
+                    file_object.close()
+
+            except IOError as e:
+                print(str(e))
+
+        else:
+            print("There is no free space currently, use the create_room \
+                  command to create new space")
 
     def confirm_existence_of_allocations(self):
         """
 
         :return:
         """
-        return True
-
+        # rooms_list = [{},{}]
+        if len(Amity.rooms_list[0]) > 0:
+            items_list = Amity.rooms_list[0].values()
+            for item in items_list:
+                if len(item) > 0:
+                    return True
+        if len(Amity.rooms_list[1]) > 0:
+            items_list = Amity.rooms_list[1].values()
+            for item in items_list:
+                if len(item) > 0:
+                    return True
+        return False
 
     def print_allocations(self, allocated_file_name=""):
-
 
         return "print_allocations() was called successfully"
 
@@ -144,7 +286,7 @@ class Amity(object):
         return False
 
     def print_unallocated(self, unallocated_file_name=""):
- 
+
         return "print_unallocated() was called successfully"
 
     def print_room(self, room_name):
@@ -154,7 +296,7 @@ class Amity(object):
         :return:
         """
 
-        return "print_room() was called successfully with arg "+room_name
+        return "print_room() was called successfully with arg " + room_name
 
     def save_state(self, destination_db=""):
         """
@@ -162,7 +304,7 @@ class Amity(object):
         :param destination_db:
         :return:
         """
-        return "save_state() was called successfully with arg "+destination_db
+        return "save_state() was called successfully with arg " + destination_db
 
     def load_state(self, source_db):
         """
@@ -171,5 +313,4 @@ class Amity(object):
         :return:
         """
 
-        return "load_state() was called successfully with arg "+source_db
- 
+        return "load_state() was called successfully with arg " + source_db
