ca03bf27d3b463b5b09a2370db7516e7a2a869f6
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-04-02 15:34:00 +0300

    restructure class

--- amity_app/classes/office.py
+++ amity_app/classes/office.py
@@ -1,29 +1,31 @@
 from amity_app.classes.room import Room
 from amity_app.classes.amity import Amity
 
+
 class Office(Room):
     """
-    child class
+    child class of class Room
+    defines a room that is an office
     """
 
-    def __init__(self, room_type="office",room_capacity=6):
+    def __init__(self, room_type="office", room_capacity=6):
         """
-
-        :param room_name:
-        :param room_type:
+        :param room_type: office
+        :param room_capacity: 6 per office
         """
 
         self.room_name = ""
         self.room_type = room_type
         self.room_capacity = room_capacity
 
-
+    # creates room of type office in amity
     def create_room(self, name):
         """
-
-        :return:
+        :param name: room name
+        :return: relevant message
         """
-        self.room_name = name
+
+        self.room_name = name.upper()
         Amity.rooms_list[1][self.room_name] = []
-        return "create_room() was called successfully from class Office"
+        return "Office room " + self.room_name + " was created successfully"
 
