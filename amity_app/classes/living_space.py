45771005c417c1e99a64cfa4640ae858ddc150b1
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-04-02 15:33:19 +0300

    restructure constructor

--- amity_app/classes/living_space.py
+++ amity_app/classes/living_space.py
@@ -1,13 +1,14 @@
 from amity_app.classes.room import Room
 from amity_app.classes.amity import Amity
 
+
 class LivingSpace(Room):
     """
-    child class
+    child class of class Room
+    defines a room that is a living space
     """
 
-
-    def __init__(self):
+    def __init__(self, room_type="livingspace", room_capacity=4):
 
         """
 
@@ -15,11 +16,11 @@ class LivingSpace(Room):
         """
 
         self.room_name = ""
-        self.room_type = "living_space"
-        self.room_capacity = 4
+        self.room_type = room_type
+        self.room_capacity = room_capacity
 
     def create_room(self, room_name):
         self.room_name = room_name
         Amity.rooms_list[0][self.room_name] = []
 
-        return self.room_name + " was created successfully"
+        return "Living space room " + self.room_name + " was created successfully"
