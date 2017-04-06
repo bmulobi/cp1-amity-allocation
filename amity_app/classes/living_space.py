353ea0e52295d7a87c5992387e09b1d1acbbb6b2
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-04-06 15:27:12 +0300

    add implementation

--- amity_app/classes/living_space.py
+++ amity_app/classes/living_space.py
@@ -6,13 +6,14 @@ class LivingSpace(Room):
     """
     child class of class Room
     defines a room that is a living space
+    Contains create_room() the creates a room of type
+    living space
     """
 
     def __init__(self, room_type="livingspace", room_capacity=4):
-
         """
-
-        :param room_type:
+        :param room_type: sets the room type
+        :param room_capacity: sets the room capacity
         """
 
         self.room_name = ""
@@ -20,7 +21,13 @@ class LivingSpace(Room):
         self.room_capacity = room_capacity
 
     def create_room(self, room_name):
-        self.room_name = room_name
+        self.room_name = room_name.upper()
+
+        if self.room_name in Amity.rooms_list[0].keys() or\
+           self.room_name in Amity.rooms_list[1].keys():
+
+            return "Room name " + self.room_name + " already exists in the system"
+
         Amity.rooms_list[0][self.room_name] = []
 
         return "Living space room " + self.room_name + " was created successfully"
