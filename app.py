1a6f1aac2df7270a5137373ddc1e8e505bf360fc
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-04-18 09:52:41 +0300

    refactored docopt to reflect changes in code

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 
 """
-This example uses docopt with the built in cmd module to demonstrate an
-interactive room allocation application.
+
+################# AMITY ROOM ALLOCATION SYSTEM #############################
+
 Usage:
-    app.py create_room (office|living_space) <room_name>...
-    app.py add_person <first_name> <last_name> (staff|fellow) [<wants_accommodation>]
+    app.py create_room (office|livingspace) <room_name>...
+    app.py add_person <first_name> <last_name> (staff|fellow) [(y|n)]
     app.py get_person_identifier <first_name> <last_name> (staff|fellow)
     app.py see_person_allocations <person_identifier>
-    app.py see_rooms_with_space [offices|living_spaces]
+    app.py see_rooms_with_space [(offices|livingspaces)]
     app.py reallocate_person <person_identifier> <new_room_name>
     app.py load_people <file_name>
     app.py print_allocations [--o=filename]
@@ -15,6 +16,9 @@ Usage:
     app.py print_room <room_name>
     app.py save_state [--db=sqlite_database]
     app.py load_state <sqlite_database>
+    app.py see_all_people [(fellows|staff)]
+    app.py see_all_rooms [(offices|livingspaces)]
+    app.py remove_person <person_identifier>
     app.py (-i | --interactive)
     app.py (-h | --help | --version)
 
@@ -43,7 +47,7 @@ def docopt_cmd(func):
             # The DocoptExit is thrown when the args do not match.
             # We print a message to the user and the usage block.
 
-            print('Invalid Command!')
+            print('\n Invalid Command!\n')
             print(e)
             return
 
@@ -73,20 +77,34 @@ class ToDo(cmd.Cmd):
 
     @docopt_cmd
     def do_add_person(self, arg):
-        """Usage: add_person <first_name> <last_name> <role> [<wants_accommodation>]"""
+        """Usage: add_person <first_name> <last_name> (staff|fellow) [(y|n)]"""
 
         name = arg["<first_name>"] + " " + arg["<last_name>"]
-        role = arg["<role>"].upper()
-        accommodation = arg["<wants_accommodation>"].upper() if arg["<wants_accommodation>"] else "N"
+
+        if arg["staff"]:
+            role = "STAFF"
+        if arg["fellow"]:
+            role = "FELLOW"
+
+        if arg["y"]:
+            accommodation = "Y"
+        elif arg["n"]:
+            accommodation = "N"
+        else:
+            accommodation = "N"
 
         result = function.add_person(name, role, wants_accommodation=accommodation)
         print(result[1])
 
     @docopt_cmd
     def do_create_room(self, arg):
-        """Usage: create_room <room_type> <room_name>..."""
+        """Usage: create_room (office|livingspace) <room_name>..."""
 
-        room_type = arg["<room_type>"].upper()
+        if arg["office"]:
+            room_type = "OFFICE"
+        if arg["livingspace"]:
+            room_type = "LIVINGSPACE"
+
         room_names = arg["<room_name>"]
 
         result = function.create_room(room_type, room_names)
@@ -113,8 +131,6 @@ class ToDo(cmd.Cmd):
     @docopt_cmd
     def do_print_allocations(self, arg):
         """Usage: print_allocations [--o=filename]"""
-
-        print(arg)
 
         if arg["--o"]:
             file_name = arg["--o"]
@@ -164,11 +180,15 @@ class ToDo(cmd.Cmd):
 
     @docopt_cmd
     def do_get_person_identifier(self, arg):
-        """Usage: get_person_identifier <first_name> <last_name> <role>"""
+        """Usage: get_person_identifier <first_name> <last_name> (staff|fellow)"""
 
         first_name = arg["<first_name>"]
         last_name = arg["<last_name>"]
-        role = arg["<role>"]
+
+        if arg["staff"]:
+            role = "staff"
+        if arg["fellow"]:
+            role = "fellow"
 
         result = function.get_person_identifier(first_name, last_name, role)
 
@@ -186,6 +206,82 @@ class ToDo(cmd.Cmd):
 
         print(function.see_person_allocations(person_identifier))
 
+    @docopt_cmd
+    def do_see_rooms_with_space(self, arg):
+        """Usage: see_rooms_with_space [(offices|livingspaces)]"""
+
+        if arg["offices"]:
+            argument = "offices"
+        elif arg["livingspaces"]:
+            argument = "livingspaces"
+        else:
+            argument = ""
+
+        result = function.see_rooms_with_space(room_type=argument)
+
+        if result == "\n There are no rooms with space":
+            print(result)
+
+        elif type(result) is list and type(result[0]) is list:
+
+            if result[0]:
+                print("\n Living spaces with available space\n -----------------------------------------")
+                for room in result[0]:
+                    print("\n ", room)
+
+            if result[1]:
+                print("\n Offices with available space\n -----------------------------------------")
+                for room in result[1]:
+                    print("\n ", room)
+        else:
+            if arg["offices"]:
+                print("\n Offices with available space\n -----------------------------------------")
+                for room in result:
+                    print("\n ", room)
+            if arg["livingspaces"]:
+                print("\n Living spaces with available space\n -----------------------------------------")
+                for room in result:
+                    print("\n ", room)
+
+    @docopt_cmd
+    def do_see_all_people(self, arg):
+        """Usage: see_all_people [(fellows|staff)]"""
+
+        if arg["fellows"]:
+            argument = "fellows"
+        elif arg["staff"]:
+            argument = "staff"
+        else:
+            argument = ""
+
+        result = function.see_all_people(role=argument)
+
+        if argument == "fellows" and result:
+            for name in result:
+                print(name)
+
+        elif argument == "fellows" and not result:
+            print("\n There are no fellows in the system")
+
+        elif argument == "staff" and result:
+            for name in result:
+                print(name)
+
+        elif argument == "staff" and not result:
+            print("\n There are no staff in the system")
+
+        elif len(result) and (result[0] or result[1]):
+            for name in result[0]:
+                print(name)
+            for name in result[1]:
+                print(name)
+
+        else:
+            print("\n There are no people in the system")
+
+
+
+
 
 
 
@@ -194,7 +290,7 @@ class ToDo(cmd.Cmd):
     def do_quit(self, arg):
         """Quits out of Interactive Mode."""
 
-        print("\nExiting Amity Room Allocation and destroying you current Session\n")
+        print("\n Exiting Amity Room Allocation and destroying your current Session\n")
         exit()
 
 opt = docopt(__doc__, sys.argv[1:])
@@ -205,4 +301,4 @@ if __name__ == "__main__":
         ToDo().cmdloop()
     except KeyboardInterrupt:
         os.system("clear")
-        print('Application Exiting')
+        print('\n Application Exiting')
